a04f868df0b30d9d2ace596516f4fa6be8da2084
Benson Mose <mosebemos26@gmail.com>
2025-12-12 10:00:11 +0300

    Configure settings and CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    pass
+    # We can add fields like is_member, is_student later if needed
+    # For now, just inheriting allows us to extend it easily in the future
